5463a3ba26894cb84ed90b692ec76965a8321ca5
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-14 23:49:13 +0530

    Update 1.3.0c_to_f.c

--- 1.3.0c_to_f.c
+++ 1.3.0c_to_f.c
@@ -3,7 +3,7 @@ void main()
 {
     float c,f;
     printf("Enter the temperature in Celsius\n");
-    scanf("%f",& c);
+    scanf("%f",&c);
     f=(c*9/5)+32;
     printf("Temperature in farhenheit is %f", f);
 }
