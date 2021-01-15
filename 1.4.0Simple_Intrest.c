392da72bfefd2fb82562a142aa3ce93015ef8d89
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-15 21:22:54 +0530

    Update 1.4.0Simple_Intrest.c

--- 1.4.0Simple_Intrest.c
+++ 1.4.0Simple_Intrest.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter Principl Ammount: ");
     scanf("%f",&p);
     printf("Enter Rate: ");
-    scanf("%f", &r);
+    scanf("%f",&r);
     printf("Enter the time in months: ");
     scanf("%f", &t);
     printf("The Simple Intrest calculated is %f", (p*r*t)/100);
