ca93c79110c6aa2816f1278cb5ddac73ad5f7ae3
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-15 21:23:33 +0530

    Update 1.4.0Simple_Intrest.c

--- 1.4.0Simple_Intrest.c
+++ 1.4.0Simple_Intrest.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     float p,r,t;
-    printf("Enter Principl Ammount: ");
+    printf("Enter the Principl Ammount: ");
     scanf("%f",&p);
-    printf("Enter Rate: ");
+    printf("Enter the Rate: ");
     scanf("%f",&r);
     printf("Enter the time in months: ");
     scanf("%f",&t);
