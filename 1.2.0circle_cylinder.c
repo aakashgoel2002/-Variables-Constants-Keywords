c9952c013e0338e250fa8dbe5246e8c2ead9b3af
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-16 20:20:29 +0530

    Update 1.2.0circle_cylinder.c

--- 1.2.0circle_cylinder.c
+++ 1.2.0circle_cylinder.c
@@ -4,8 +4,8 @@ int main(){
     printf("Enter the Radius of the Circle: ");
     scanf("%f",&r);
     printf("The Area of the Circle is: %f\n", r*r*pi );
-    printf("Enter the Height of the cylinder: ");
+    printf("Enter the Height of the Cylinder: ");
     scanf("%f",&h);
-    printf("The volume of cylinder is: %f\n", pi*r*r*h);
+    printf("The Volume of Cylinder is: %f\n", pi*r*r*h);
     return 0;
 }
