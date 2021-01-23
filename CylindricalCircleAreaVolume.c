862e0010e8a7091299a75e14a4c22298dbd0ba1e
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-23 23:36:44 +0530

    Update CylindricalCircleAreaVolume.c

--- CylindricalCircleAreaVolume.c
+++ CylindricalCircleAreaVolume.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%f",&r);
     printf("The Area of the Circle is: %f\n", r*r*pi );
     printf("Enter the Height of the Cylinder: ");
-    scanf("%f",&h);
+    scanf("%f",&h)
     printf("The Volume of Cylinder is: %f\n", pi*r*r*h);
     return 0;
 }
