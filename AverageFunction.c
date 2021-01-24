1289d80913421605b9bd88222680e0a06cbe8426
Aakash Goel <65633502+aakashgoel2002@users.noreply.github.com>
2021-01-24 23:41:24 +0530

    Update AverageFunction.c

--- AverageFunction.c
+++ AverageFunction.c
@@ -9,7 +9,7 @@ int main(){
     avg=average(a,b,c);         // finding the average
     printf("The Average of the Numbers entered above is: %f",avg);
     return 0;                   
-}
+
 float average(float a,float b,float c)//function to find average
 {
     float avg=(a+b+c)/3.00;     //calculating average
