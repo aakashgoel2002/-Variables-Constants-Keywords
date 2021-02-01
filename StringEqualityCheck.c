6e15452e8bb4e879a9472cff596b0d0c791fd672
Aakash Goel <aakashgoel2002@gmail.com>
2021-02-01 23:24:39 +0530

    Create StringEqualityCheck.c

---
+++ StringEqualityCheck.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char st[100];
+    char sp[100];
+    printf("Enter 2 Strings: \n");
+    gets(st);
+    gets(sp);
+    int s=strcmp(st,sp);
+    if(s==0)
+    printf("Both are Equal");
+    else
+    printf("They are Unequal");
+    return 0;
+}
