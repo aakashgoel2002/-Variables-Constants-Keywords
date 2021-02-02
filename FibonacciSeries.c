e189a843fc61a69a22ae46a5a24e4d26b503da0b
Aakash Goel <aakashgoel2002@gmail.com>
2021-02-02 23:16:52 +0530

    Create FibonacciSeries.c

---
+++ FibonacciSeries.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fib(int n);
+int main()
+{
+    int n;
+    printf("Enter the value of Nth element\n");
+    scanf("%d",&n);
+    printf("The Value of Fibonacci series at nTH element is %d",fib(n));
+}
+int fib(int n)
+{
+    if(n==0)
+    return 0;
+    else if(n==1)
+    return 1;
+    else
+    {
+        int ret=fib(n-1)+fib(n-2);
+        return ret;
+    }
+}
