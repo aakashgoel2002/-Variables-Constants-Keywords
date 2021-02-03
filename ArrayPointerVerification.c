85f1e7288980d2260422d802b2339d1227991b68
Aakash Goel <aakashgoel2002@gmail.com>
2021-02-03 22:32:20 +0530

    Create ArrayPointerVerification.c

---
+++ ArrayPointerVerification.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10];
+    int *ptr=arr;
+    ptr=ptr+2;
+    if(&arr[2]==ptr)
+    printf("These Points have same memory location\n");
+    else
+    printf("These do not have same memory location\n");
+    return 0;
+}
